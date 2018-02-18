7847ab9aacad917dcb91ada6fee2d2bb1b9427f6
SMagnet22 <33917053+SMagnet22@users.noreply.github.com>
2018-02-18 10:18:20 -0500

    Add files via upload

---
+++ StringRec.java
@@ -0,0 +1,29 @@
+
+public class StringRec {
+
+	public static void main(String[] args) {
+		
+		System.out.println(decompress("4q9w5e2rt5y4qw2Er3T"));
+	}
+
+	public static String decompress(String compressedText) {
+		if (compressedText.length()==0 || compressedText.length()==1) {
+			return compressedText;
+		}
+		else {
+			if (Character.isLetter(compressedText.charAt(0))) {
+				return compressedText.charAt(0) + decompress(compressedText.substring(1));
+			}else {
+				if (compressedText.charAt(0)=='0') {
+					return decompress(compressedText.substring(2));
+				}else {
+					char c = compressedText.charAt(0);
+					char x = compressedText.charAt(1);
+					int value = c-'0';
+					compressedText= Integer.toString(value-1) + x + compressedText.substring(2);
+					return x + decompress(compressedText);
+				}
+			}
+		}
+	}
+}
